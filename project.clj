7b6c2fe3a651878a3c0ff82eb55f430cad2f697c
Dé <rahul080327@gmail.com>
2016-08-15 17:59:39 +0530

    R/J | Add sample CSRF keygen auth for POST requests

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-anti-forgery "1.0.1"]
+                 [cheshire "5.6.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler codeprix.handler/app}
   :profiles
--- src/codeprix/handler.clj
+++ src/codeprix/handler.clj
@@ -1,7 +1,11 @@
 (ns codeprix.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.anti-forgery :refer :all]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.session.cookie :refer (cookie-store)]
+            [cheshire.core :refer :all]))
 (defn add
   [a b]
   (+ a b))
@@ -9,7 +13,12 @@
 (defroutes app-routes
   (GET "/name/:name" [name] (str "Hello " name))
   (GET "/add/:a/:b" [a b] (str (add (Integer. a) (Integer. b))))
+  (GET "/" [] (generate-string {:csrf-token *anti-forgery-token*}))
+  (POST "/callback" {params :params} (str (pr-str params)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+  (wrap-defaults site-defaults)
+  (wrap-session {:cookie-attrs {:max-age 3600}
+                 :store (cookie-store {:key "ahY9poQuaghahc7I"})})))
