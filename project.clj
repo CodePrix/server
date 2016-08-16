77cc05e115099b881ad57e6c25a932944c5fc2f8
Dé <rahul080327@gmail.com>
2016-08-16 10:19:38 +0530

    R/J | Remove CSRF deps, made api mode from site

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject codeprix "0.1.0-SNAPSHOT"
   :description "The Server Code for CodePrix"
-  :url "https://github.com/JyotsnaGorle/CodePrix"
+  :url "https://github.com/CodePrix"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [ring/ring-anti-forgery "1.0.1"]
-                 [cheshire "5.6.3"]]
+                 [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler codeprix.handler/app}
   :profiles
--- src/codeprix/handler.clj
+++ src/codeprix/handler.clj
@@ -1,11 +1,7 @@
 (ns codeprix.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.anti-forgery :refer :all]
-            [ring.middleware.session :refer [wrap-session]]
-            [ring.middleware.session.cookie :refer (cookie-store)]
-            [cheshire.core :refer :all]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 (defn add
   [a b]
   (+ a b))
@@ -13,12 +9,9 @@
 (defroutes app-routes
   (GET "/name/:name" [name] (str "Hello " name))
   (GET "/add/:a/:b" [a b] (str (add (Integer. a) (Integer. b))))
-  (GET "/" [] (generate-string {:csrf-token *anti-forgery-token*}))
-  (POST "/callback" {params :params} (str (pr-str params)))
+  (GET "/" [] "Server is Running!")
+  (POST "/callback" {params :params} (pr-str params))
   (route/not-found "Not Found"))
 
 (def app
-  (-> app-routes
-  (wrap-defaults site-defaults)
-  (wrap-session {:cookie-attrs {:max-age 3600}
-                 :store (cookie-store {:key "ahY9poQuaghahc7I"})})))
+  (wrap-defaults app-routes api-defaults))
